036a3f788ff2467653c8481dcd3d857aa261facc
Dmitry Salychev <darkness.bsd@gmail.com>
2017-02-16 11:48:26 +0100

    tsc: Test for msim_median().

--- tests/math_test.c
+++ tests/math_test.c
@@ -1,29 +1,37 @@
+#include <stdint.h>
+#include <inttypes.h>
+
 #include "minunit.h"
 #include "tools/math/math.h"
 
 #define SUITE_NAME		"Math tests"
 
+#define ARR_OVERSIZE		4097
+
 /* Tests counter */
 int tests_run = 0;
 
+static uint64_t huge_arr[ARR_OVERSIZE];
+
 /* Test functions prototypes */
 int calc_median(void);
-int calc_medianf(void);
 
 int calc_median(void)
 {
-	return 0;
-}
+	uint64_t a[] = { 1, 2, 3 };
+	uint64_t b[] = { 10, 10, 20, 30 };
 
-int calc_medianf(void)
-{
+	_mu_test(msim_median(a, sizeof(a)/sizeof(a[0])) == 2);
+	_mu_test(msim_median(b, sizeof(b)/sizeof(b[0])) == 15);
+	_mu_test(msim_median(huge_arr, sizeof(huge_arr)/sizeof(huge_arr[0]))
+			== UINT64_MAX);
+
 	return 0;
 }
 
 int all_tests(void)
 {
 	_mu_verify(calc_median);
-	_mu_verify(calc_medianf);
 	return 0;
 }
 
