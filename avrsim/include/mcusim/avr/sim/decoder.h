203e8c4ffe61e1573bba1811e73f877b66e307cd
Dmitry Salychev <darkness.bsd@gmail.com>
2017-09-14 17:59:14 +0200

    avrsim: Add decoder for AVR instructions.
    	- decoder.c contains all opcode executors and export
    	  MSIM_StepAVR() to run next instruction.

---
+++ avrsim/include/mcusim/avr/sim/decoder.h
@@ -0,0 +1,35 @@
+/*
+ * AVRSim - Simulator for AVR microcontrollers.
+ * This software is a part of MCUSim, interactive simulator for
+ * microcontrollers.
+ * Copyright (C) 2017 Dmitry Salychev <darkness.bsd@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef MSIM_AVR_DECODER_H_
+#define MSIM_AVR_DECODER_H_ 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int MSIM_StepAVR(struct MSIM_AVR *mcu);
+
+int MSIM_Is32(unsigned int inst);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MSIM_AVR_DECODER_H_ */
