25aa5aa9e3b5ebc44bceb8570f710f6fa2fdd4af
Dmitry Salychev <darkness.bsd@gmail.com>
2017-12-09 13:21:02 +0100

    patch 0.1.26: Add standard I/O registers of ATmega2560 to VCD.

--- avrsim/include/mcusim/avr/sim/vcd_regs.h
+++ avrsim/include/mcusim/avr/sim/vcd_regs.h
@@ -25,6 +25,7 @@
  * another one - doesn't. Memory offset of the unavailable registers
  * will be marked as NO_REG.
  */
+/* Status and stack registers */
 #ifndef SREG
 #define SREG		NO_REG
 #endif
@@ -35,6 +36,20 @@
 #define SPL		NO_REG
 #endif
 
+/*
+ * General purpose I/O ports.
+ */
+/* Port A registers */
+#ifndef PORTA
+#define PORTA		NO_REG
+#endif
+#ifndef DDRA
+#define DDRA		NO_REG
+#endif
+#ifndef PINA
+#define PINA		NO_REG
+#endif
+/* Port B registers */
 #ifndef PORTB
 #define PORTB		NO_REG
 #endif
@@ -44,7 +59,7 @@
 #ifndef PINB
 #define PINB		NO_REG
 #endif
-
+/* Port C registers */
 #ifndef PORTC
 #define PORTC		NO_REG
 #endif
@@ -54,7 +69,7 @@
 #ifndef PINC
 #define PINC		NO_REG
 #endif
-
+/* Port D registers */
 #ifndef PORTD
 #define PORTD		NO_REG
 #endif
@@ -64,6 +79,39 @@
 #ifndef PIND
 #define PIND		NO_REG
 #endif
+/* Port E registers */
+#ifndef PORTE
+#define PORTE		NO_REG
+#endif
+#ifndef DDRE
+#define DDRE		NO_REG
+#endif
+#ifndef PINE
+#define PINE		NO_REG
+#endif
+/* Port F registers */
+#ifndef PORTF
+#define PORTF		NO_REG
+#endif
+#ifndef DDRF
+#define DDRF		NO_REG
+#endif
+#ifndef PINF
+#define PINF		NO_REG
+#endif
+/* Port G registers */
+#ifndef PORTG
+#define PORTG		NO_REG
+#endif
+#ifndef DDRG
+#define DDRG		NO_REG
+#endif
+#ifndef PING
+#define PING		NO_REG
+#endif
+/*
+ * END General purpose I/O ports.
+ */
 
 #ifndef EIND
 #define EIND		NO_REG
@@ -95,6 +143,79 @@
 #ifndef SPSR
 #define SPSR		NO_REG
 #endif
+#ifndef SPCR
+#define SPCR		NO_REG
+#endif
+#ifndef GPIOR2
+#define GPIOR2		NO_REG
+#endif
+#ifndef GPIOR1
+#define GPIOR1		NO_REG
+#endif
+#ifndef GPIOR0
+#define GPIOR0		NO_REG
+#endif
+
+/* Timer/Counter0 */
+#ifndef OCR0B
+#define OCR0B		NO_REG
+#endif
+#ifndef OCR0A
+#define OCR0A		NO_REG
+#endif
+#ifndef TCNT0
+#define TCNT0		NO_REG
+#endif
+#ifndef TCCR0B
+#define TCCR0B		NO_REG
+#endif
+#ifndef TCCR0A
+#define TCCR0A		NO_REG
+#endif
+/* END Timer/Counter0 */
+
+#ifndef GTCCR
+#define GTCCR		NO_REG
+#endif
+#ifndef EEARH
+#define EEARH		NO_REG
+#endif
+#ifndef EEARL
+#define EEARL		NO_REG
+#endif
+#ifndef EEDR
+#define EEDR		NO_REG
+#endif
+#ifndef EECR
+#define EECR		NO_REG
+#endif
+#ifndef EIMSK
+#define EIMSK		NO_REG
+#endif
+#ifndef EIFR
+#define EIFR		NO_REG
+#endif
+#ifndef PCIFR
+#define PCIFR		NO_REG
+#endif
+#ifndef TIFR5
+#define TIFR5		NO_REG
+#endif
+#ifndef TIFR4
+#define TIFR4		NO_REG
+#endif
+#ifndef TIFR3
+#define TIFR3		NO_REG
+#endif
+#ifndef TIFR2
+#define TIFR2		NO_REG
+#endif
+#ifndef TIFR1
+#define TIFR1		NO_REG
+#endif
+#ifndef TIFR0
+#define TIFR0		NO_REG
+#endif
 
 /*
  * Available MCU registers to include into VCD dump.
@@ -106,6 +227,10 @@
 	{ "SREG",	SREG, NULL, 0 },				\
 	{ "SPH",	SPH, NULL, 0 },					\
 	{ "SPL",	SPL, NULL, 0 },					\
+									\
+	{ "PORTA",	PORTA, NULL, 0 },				\
+	{ "DDRA",	DDRA, NULL, 0 },				\
+	{ "PINA",	PINA, NULL, 0 },				\
 	{ "PORTB",	PORTB, NULL, 0 },				\
 	{ "DDRB",	DDRB, NULL, 0 },				\
 	{ "PINB",	PINB, NULL, 0 },				\
@@ -115,6 +240,16 @@
 	{ "PORTD",	PORTD, NULL, 0 },				\
 	{ "DDRD",	DDRD, NULL, 0 },				\
 	{ "PIND",	PIND, NULL, 0 },				\
+	{ "PORTE",	PORTE, NULL, 0 },				\
+	{ "DDRE",	DDRE, NULL, 0 },				\
+	{ "PINE",	PINE, NULL, 0 },				\
+	{ "PORTF",	PORTF, NULL, 0 },				\
+	{ "DDRF",	DDRF, NULL, 0 },				\
+	{ "PINF",	PINF, NULL, 0 },				\
+	{ "PORTG",	PORTG, NULL, 0 },				\
+	{ "DDRG",	DDRG, NULL, 0 },				\
+	{ "PING",	PING, NULL, 0 },				\
+									\
 	{ "EIND",	EIND, NULL, 0 },				\
 	{ "RAMPZ",	RAMPZ, NULL, 0 },				\
 	{ "SPMCSR",	SPMCSR, NULL, 0 },				\
@@ -124,5 +259,30 @@
 	{ "OCDR",	OCDR, NULL, 0 },				\
 	{ "ACSR",	ACSR, NULL, 0 },				\
 	{ "SPDR",	SPDR, NULL, 0 },				\
-	{ "SPSR",	SPSR, NULL, 0 }					\
+	{ "SPSR",	SPSR, NULL, 0 },				\
+	{ "SPCR",	SPCR, NULL, 0 },				\
+	{ "GPIOR2",	GPIOR2, NULL, 0 },				\
+	{ "GPIOR1",	GPIOR1, NULL, 0 },				\
+	{ "GPIOR0",	GPIOR0, NULL, 0 },				\
+									\
+	{ "OCR0B",	OCR0B, NULL, 0 },				\
+	{ "OCR0A",	OCR0A, NULL, 0 },				\
+	{ "TCNT0",	TCNT0, NULL, 0 },				\
+	{ "TCCR0B",	TCCR0B, NULL, 0 },				\
+	{ "TCCR0A",	TCCR0A, NULL, 0 },				\
+									\
+	{ "GTCCR",	GTCCR, NULL, 0 },				\
+	{ "EEARH",	EEARH, NULL, 0 },				\
+	{ "EEARL",	EEARL, NULL, 0 },				\
+	{ "EEDR",	EEDR, NULL, 0 },				\
+	{ "EECR",	EECR, NULL, 0 },				\
+	{ "EIMSK",	EIMSK, NULL, 0 },				\
+	{ "EIFR",	EIFR, NULL, 0 },				\
+	{ "PCIFR",	PCIFR, NULL, 0 },				\
+	{ "TIFR5",	TIFR5, NULL, 0 },				\
+	{ "TIFR4",	TIFR4, NULL, 0 },				\
+	{ "TIFR3",	TIFR3, NULL, 0 },				\
+	{ "TIFR2",	TIFR2, NULL, 0 },				\
+	{ "TIFR1",	TIFR1, NULL, 0 },				\
+	{ "TIFR0",	TIFR0, NULL, 0 }				\
 }
