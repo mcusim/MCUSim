ecbf1fba1fc380abbf5d04b0468babdd603ea745
Dmitry Salychev <darkness.bsd@gmail.com>
2019-04-15 10:15:30 +0200

    patch 0.1.170: Add toggle pin test for m328p.
    ticket: N/A
    
    See tests/ATMEGA328-toggle-pin for details.
    
    This test shows why it is not a good idea to use _delay_ms() function to
    perform a delay. Delay is supposed to be ~200ms, but it is ~159ms
    actually.
    
    Firmware of the test should actually be checked in a real circuit.

--- tests/ATMEGA328-toggle-pin/check-pin.lua
+++ tests/ATMEGA328-toggle-pin/check-pin.lua
@@ -24,52 +24,48 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
-
-This script checks for a simple pattern of port outputs.
 --]]
 
-VERBOSE = true			-- Switch on to enable verbose output
-TICK_TIME = 0.0			-- clock period, in us
-
-ticks_passed = 0
-check_point = 0			-- PORTB is 0 at start, count up to 255 then
-				-- and reset by overflow interrupt
+HALF_PERIOD = 1272330 -- in 8MHz clock cycles
+ticks = 0
+pb0 = 0
+change = 0
+change_tick = 0
 
 -- This function will be called by the simulator only once to configure
 -- model before start of a simulation.
 function module_conf(mcu)
-	-- Re-calculate clock period, in us
-	TICK_TIME = (1.0/MSIM_Freq(mcu))*1000000.0
-	if VERBOSE then
-		print("[PORTB checker] MCU clock: " .. MSIM_Freq(mcu)/1000 ..
-		      "kHz")
-		print("[PORTB checker] MCU clock period: " .. TICK_TIME ..
-		      "us")
-	end
 end
 
 -- This function will be called by the simulator periodically according to the
 -- main simulation loop, i.e. time passed between two neighbor calls to the
 -- function is a period.
 function module_tick(mcu)
-	if check_point == 0 and AVR_ReadIO(mcu, PORTB) == 0 then
-		print("[PORTB checker] check 1")
-		check_point = check_point + 1
-	elseif check_point == 1 and AVR_ReadIO(mcu, PORTB) == 255 then
-		print("[PORTB checker] check 2")
-		check_point = check_point + 1
-	elseif check_point == 2 and AVR_ReadIO(mcu, PORTB) == 254 then
-		print("[PORTB checker] check 3")
-		check_point = check_point + 1
-	elseif check_point == 3 and AVR_ReadIO(mcu, PORTB) == 255 then
-		-- Test finished successfully
-		MSIM_SetState(mcu, AVR_MSIM_STOP)
-		print("[PORTB checker] ticks passed: " .. ticks_passed)
-	elseif ticks_passed > 200 then
-		-- Test failed
-		MSIM_SetState(mcu, AVR_MSIM_TESTFAIL)
-		print("[PORTB checker] ticks passed: " .. ticks_passed)
+	if AVR_IOBit(mcu, PORTB, 0) ~= pb0 then
+		pb0 = AVR_IOBit(mcu, PORTB, 0)
+		if change == 2 and (ticks-change_tick) == HALF_PERIOD then
+			print("[check-pin] tick:" .. ticks ..
+			      " previous tick:" .. change_tick ..
+			      " delta:" .. ticks-change_tick)
+		elseif change == 6 and (ticks-change_tick) == HALF_PERIOD then
+			print("[check-pin] tick:" .. ticks ..
+			      " previous tick:" .. change_tick ..
+			      " delta:" .. ticks-change_tick)
+		elseif change == 9 and (ticks-change_tick) == HALF_PERIOD then
+			print("[check-pin] tick:" .. ticks ..
+			      " previous tick:" .. change_tick ..
+			      " delta:" .. ticks-change_tick)
+			-- Test finished successfully
+			MSIM_SetState(mcu, AVR_MSIM_STOP)
+		elseif change == 10 then
+			-- Test failed
+			MSIM_SetState(mcu, AVR_MSIM_TESTFAIL)
+			print("[check-pin] ticks passed: " .. ticks)
+		end
+
+		change = change+1
+		change_tick = ticks
 	end
 
-	ticks_passed = ticks_passed + 1
+	ticks = ticks+1
 end
